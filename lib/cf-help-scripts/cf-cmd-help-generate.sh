008d77c70955ead7405dea6909b00ceb1902b3f2
Dies Koper <diesk@fast.au.fujitsu.com>
2016-03-04 08:37:53 +1100

    removed index.html from links to index pages to get nicer looking urls

--- lib/cf-help-scripts/cf-cmd-help-generate.sh
+++ lib/cf-help-scripts/cf-cmd-help-generate.sh
@@ -18,7 +18,7 @@ sed -i -e "s,LOCALE,$LOCALE,g" $TARGET_DIR/$CMD.html
 sed -i -e "s,\(li\)\(..a href.*%LOCALE%\),\1 id=\"current-lang\"\2,i" %TARGET_DIR%\$CMD.html
 
 #update link for other languages to this command
-sed -i -e "s,/cf/index.html,/cf/$CMD.html,i" $TARGET_DIR/$CMD.html
+sed -i -e "s,cf\"\(\">\w\),cf/$CMD.html\1,i" $TARGET_DIR/$CMD.html
 
 #revert the one for Home
 sed -i -e "s,$LOCALE/cf/$CMD.html,$LOCALE/cf/index.html,i" $TARGET_DIR/$CMD.html
