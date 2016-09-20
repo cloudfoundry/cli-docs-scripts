ba5aba5958b75e51ca37443f9adbb7a18cb24aa8
Dies Koper <diesk@fast.au.fujitsu.com>
2016-09-20 19:57:39 +1000

    added `-a` option to retrieve full help page (to be introduced in cf CLI 6.22.0)

--- lib/cf-help-scripts/cf-cmd-list-all-langs.sh
+++ lib/cf-help-scripts/cf-cmd-list-all-langs.sh
@@ -11,7 +11,7 @@ fi
 export PATH=.:$PATH
 
 cf -v > cf-version.txt
-cf h | sed -n '/GETTING/,/ENVIRONMENT VARIABLES:/{/GETTING/b;/ENVIRONMENT VARIABLES:/b;p}' | sed -r '/.*:$/d;/^^$/d;s/   ([a-z-]+).*$/\1/' > cf-cmd-list.txt
+cf h -a | sed -n '/GETTING/,/ENVIRONMENT VARIABLES:/{/GETTING/b;/ENVIRONMENT VARIABLES:/b;p}' | sed -r '/.*:$/d;/^^$/d;s/   ([a-z-]+).*$/\1/' > cf-cmd-list.txt
 
 for LOCALE in `./cf config --locale DUMMY | tail -n+3 | tr _ -`; do
 #for LOCALE in `echo en_US`; do
