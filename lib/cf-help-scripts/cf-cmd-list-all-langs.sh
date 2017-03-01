ef5c063822e6ac40fb538d86304ed74fc0b8e912
Max Hufnagel <mhufnagel@pivotal.io>
2017-02-28 17:42:33 -0800

    Update cf-cmd-list-all-langs.sh

--- lib/cf-help-scripts/cf-cmd-list-all-langs.sh
+++ lib/cf-help-scripts/cf-cmd-list-all-langs.sh
@@ -14,7 +14,8 @@ cf config --color false
 cf -v > cf-version.txt
 cf h -a | sed -n '/GETTING/,/ENVIRONMENT VARIABLES:/{/GETTING/b;/ENVIRONMENT VARIABLES:/b;p}' | sed -r '/.*:$/d;/^^$/d;s/   ([a-z-]+).*$/\1/' | sed '/.?*\:/d' > cf-cmd-list.txt
 
-for LOCALE in `./cf config --locale DUMMY | tail -n+3 | tr _ -`; do
+for LOCALE in "de-DE en-US es-ES fr-FR it-IT ja-JP ko-KR pt-BR zh-Hans zh-Hant"; do 
+#Non-programmer replaces the following with the above: for LOCALE in `./cf config --locale DUMMY | tail -n+3 | tr _ -`; do
 #for LOCALE in `echo en_US`; do
 
 echo Processing language: $LOCALE
