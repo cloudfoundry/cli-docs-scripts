d07a6589aa07910e47f859d4b46b72654a86abb2
Dies Koper <diesk@fast.au.fujitsu.com>
2017-09-23 08:59:53 +1000

    include experimental commands at end of help

--- lib/cf-help-scripts/cf-cmd-list-all-langs.sh
+++ lib/cf-help-scripts/cf-cmd-list-all-langs.sh
@@ -12,7 +12,9 @@ export PATH=.:$PATH
 
 cf config --color false
 cf -v > cf-version.txt
+# search for, then print range from GETTING to VARIABLES | delete section headers (lines ending with ':') ; delete empty lines ; remove whitespace and text around command
 cf h -a | sed -n '/GETTING/,/ENVIRONMENT VARIABLES:/{/GETTING/b;/ENVIRONMENT VARIABLES:/b;p}' | sed -r '/.*:$/d;/^^$/d;s/   ([a-z-]+).*$/\1/' | sed '/.?*\:/d' > cf-cmd-list.txt
+cf h -a | sed -n '/experimental/,${/experimental/b;$b;p}' | sed -r '/.*:$/d;/^^$/d;s/   ([a-z0-9-]+) .*$/\1/'  >> cf-cmd-list.txt
 
 for LOCALE in de-DE en-US es-ES fr-FR it-IT ja-JP ko-KR pt-BR zh-Hans zh-Hant; do 
 #Non-programmer replaces the following with the above: for LOCALE in `./cf config --locale DUMMY | tail -n+3 | tr _ -`; do
