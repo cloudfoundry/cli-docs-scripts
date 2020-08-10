f5c5db66765edbab8ab59573877eb0cf6e7a03ca
Nick Webb <nwebb@pivotal.io>
2020-08-10 14:59:15 -0700

    Introduce scripts to fly execute docs pipeline tasks
    [#174237374](https://www.pivotaltracker.com/story/show/174237374)
    
    Signed-off-by: Steve Taylor <staylor@pivotal.io>
    Co-authored-by: Steve Taylor <staylor@pivotal.io>

---
+++ bin/fly_generate_docs_cloudfoundry_org.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+CONCOURSE_SCRIPTS_DIR=/home/aberezovsky/workspace/concourse-scripts-docs
+CLI_DOCS_SCRIPTS_DIR=/home/aberezovsky/workspace/cli-docs-scripts
+CLI_RELEASE_DIR=/tmp/cf-cli-release
+DOCS_CF_CLI=/home/aberezovsky/workspace/docs-cf-cli
+OUT_DOCS_CF_CLI=/tmp/docs-cf-cli
+
+function main() {
+  rm -rf $OUT_DOCS_CF_CLI && mkdir -p $OUT_DOCS_CF_CLI
+  fly -t ci execute \
+    --include-ignored \
+    -c $CONCOURSE_SCRIPTS_DIR/pubtools/cf-CLI/generate-cli-section-page.yml \
+    -i concourse-scripts=$CONCOURSE_SCRIPTS_DIR \
+    -i cf-cli-release=$CLI_RELEASE_DIR \
+    -i cli-docs-scripts=$CLI_DOCS_SCRIPTS_DIR \
+    -i docs-cf-cli-in=$DOCS_CF_CLI \
+    -o docs-cf-cli-out=$OUT_DOCS_CF_CLI
+}
+
+main "$@"
